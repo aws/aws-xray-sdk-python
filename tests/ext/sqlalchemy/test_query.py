d110386f72f6e25d3a2aa76d3440b734eb2840c5
therealryanbonham <33603723+therealryanbonham@users.noreply.github.com>
2018-02-19 19:17:53 -0600

    Add Tracing for SQLAlchemy and Flask-SQLAlcemy (#14)
    * Initial checkin of Query and BaseQuery overrides
    
    * Fix ext name
    
    * Fix import
    
    * Add support for SQLAlchemy.orm and Flask-SQLAlchemy
    
    * Remove print() statement
    
    * Attempt to fix handling of Flask not having a request with a xray segment
    
    * Fix handling of missing segment
    
    * Fix test and add docstrings
    
    * Fix bug with End segment
    
    * Code Review Cleanup. Files now all pass flake8 tests
    
    * Move find_subsegment and _search_entity functions to tests/util.py
    
    * Uset set_sql to corectly test the sanitized_query value. Add test to sqlalcemy to test filter() and verify params not present in sanitized_query
    
    * Comment out set_sql for sanitized_query for seperate code review
    
    * Starting to add in set_sql
    
    * Add more SQL info to trace
    
    * Correct URL handling for connection strings
    
    * Bug fix and remove sanitized_query
    
    * Fix unit test and add helper util for finding subsegment by annotation key/value
    
    * Minor cleanups

---
+++ tests/ext/sqlalchemy/test_query.py
@@ -0,0 +1,69 @@
+from __future__ import absolute_import
+import pytest
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core.context import Context
+from aws_xray_sdk.ext.sqlalchemy.query import XRaySessionMaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String
+from ...util import find_subsegment_by_annotation
+
+
+Base = declarative_base()
+
+
+class User(Base):
+        __tablename__ = 'users'
+
+        id = Column(Integer, primary_key=True)
+        name = Column(String)
+        fullname = Column(String)
+        password = Column(String)
+
+
+@pytest.fixture()
+def session():
+    """Test Fixture to Create DataBase Tables and start a trace segment"""
+    engine = create_engine('sqlite:///:memory:')
+    xray_recorder.configure(service='test', sampling=False, context=Context())
+    xray_recorder.clear_trace_entities()
+    xray_recorder.begin_segment('SQLAlchemyTest')
+    Session = XRaySessionMaker(bind=engine)
+    Base.metadata.create_all(engine)
+    session = Session()
+    yield session
+    xray_recorder.end_segment()
+    xray_recorder.clear_trace_entities()
+
+
+def test_all(capsys, session):
+    """ Test calling all() on get all records.
+    Verify we run the query and return the SQL as metdata"""
+    # with capsys.disabled():
+    session.query(User).all()
+    subsegment = find_subsegment_by_annotation(xray_recorder.current_segment(), 'sqlalchemy', 'sqlalchemy.orm.query.all')
+    assert subsegment['annotations']['sqlalchemy'] == 'sqlalchemy.orm.query.all'
+    # assert subsegment['sql']['sanitized_query']
+    assert subsegment['sql']['url']
+
+
+def test_add(capsys, session):
+    """ Test calling add() on insert a row.
+    Verify we that we capture trace for the add"""
+    # with capsys.disabled():
+    john = User(name='John', fullname="John Doe", password="password")
+    session.add(john)
+    subsegment = find_subsegment_by_annotation(xray_recorder.current_segment(), 'sqlalchemy', 'sqlalchemy.orm.session.add')
+    assert subsegment['annotations']['sqlalchemy'] == 'sqlalchemy.orm.session.add'
+    assert subsegment['sql']['url']
+
+
+def test_filter(capsys, session):
+    """ Test calling all() on get all records.
+    Verify we run the query and return the SQL as metdata"""
+    # with capsys.disabled():
+    session.query(User).filter(User.password=="mypassword!")
+    subsegment = find_subsegment_by_annotation(xray_recorder.current_segment(), 'sqlalchemy', 'sqlalchemy.orm.query.filter')
+    assert subsegment['annotations']['sqlalchemy'] == 'sqlalchemy.orm.query.filter'
+    # assert subsegment['sql']['sanitized_query']
+    # assert "mypassword!" not in subsegment['sql']['sanitized_query']
+    assert subsegment['sql']['url']
