9fc1b4a63a248e3d5183c1695cea37786c2e842c
Prashant Srivastava <srprash@amazon.com>
2020-05-21 00:42:02 +0530

    Implemented support for IMDSv2 and fallback to IMDSv1. Added unit tests for the same

--- aws_xray_sdk/core/plugins/ec2_plugin.py
+++ aws_xray_sdk/core/plugins/ec2_plugin.py
@@ -1,12 +1,14 @@
 import logging
 from future.standard_library import install_aliases
+from urllib.request import urlopen, Request
+
 install_aliases()
-from urllib.request import urlopen
 
 log = logging.getLogger(__name__)
 
 SERVICE_NAME = 'ec2'
 ORIGIN = 'AWS::EC2::Instance'
+IMDS_URL = 'http://169.254.169.254/latest/'
 
 
 def initialize():
@@ -17,16 +19,51 @@ def initialize():
     """
     global runtime_context
 
+    # Try the IMDSv2 endpoint for metadata
     try:
         runtime_context = {}
 
-        r = urlopen('http://169.254.169.254/latest/meta-data/instance-id', timeout=1)
-        runtime_context['instance_id'] = r.read().decode('utf-8')
+        # get session token
+        token = do_request(url=IMDS_URL + "api/token",
+                           headers={"X-aws-ec2-metadata-token-ttl-seconds": "21600"},
+                           method="PUT")
 
-        r = urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone',
-                    timeout=1)
-        runtime_context['availability_zone'] = r.read().decode('utf-8')
+        # get instance-id metadata
+        runtime_context['instance_id'] = do_request(url=IMDS_URL + "meta-data/instance-id",
+                                                    headers={"X-aws-ec2-metadata-token": token},
+                                                    method="GET")
 
+        # get availability-zone metadata
+        runtime_context['availability_zone'] = do_request(url=IMDS_URL + "meta-data/placement/availability-zone",
+                                                          headers={"X-aws-ec2-metadata-token": token},
+                                                          method="GET")
+
+    except Exception as e:
+        # Falling back to IMDSv1 endpoint
+        log.warning("failed to get ec2 instance metadata from IMDSv2 due to {}. Falling back to IMDSv1".format(e))
+
+        try:
+            runtime_context = {}
+
+            runtime_context['instance_id'] = do_request(url=IMDS_URL + "meta-data/instance-id")
+
+            runtime_context['availability_zone'] = do_request(url=IMDS_URL + "meta-data/placement/availability-zone-1")
+
+        except Exception as e:
+            runtime_context = None
+            log.warning("failed to get ec2 instance metadata from IMDSv1 due to {}".format(e))
+
+
+def do_request(url, headers=None, method="GET"):
+    if headers is None:
+        headers = {}
+    try:
+        if url is None:
+            return None
+        req = Request(url=url)
+        req.headers = headers
+        req.method = method
+        res = urlopen(req, timeout=1)
+        return res.read().decode('utf-8')
     except Exception:
-        runtime_context = None
-        log.warning("failed to get ec2 instance metadata.")
+        raise
--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,4 +1,5 @@
 from aws_xray_sdk.core.plugins.utils import get_plugin_modules
+from unittest.mock import patch
 
 supported_plugins = (
     'ec2_plugin',
@@ -13,3 +14,34 @@ def test_runtime_context_available():
     for plugin in plugins:
         plugin.initialize()
         assert hasattr(plugin, 'runtime_context')
+
+
+@patch('urllib.request.urlopen')
+def test_ec2_plugin(mock_urlopen):
+    # set up mock response
+    mock_urlopen.return_value.read.return_value.decode.side_effect = ['token', 'i-0a1d026d92d4709cd', 'us-west-2b',
+                                                                      Exception("Boom!"), 'i-0a1d026d92d4709ab',
+                                                                      'us-west-2a',
+                                                                      Exception("Boom v2!"), Exception("Boom v1!")]
+
+    ec2_plugin = get_plugin_modules(('ec2_plugin',))
+    for plugin in ec2_plugin:
+        # for IMDSv2 success
+        plugin.initialize()
+        assert hasattr(plugin, 'runtime_context')
+        r_c = getattr(plugin, 'runtime_context')
+        assert r_c['instance_id'] == 'i-0a1d026d92d4709cd'
+        assert r_c['availability_zone'] == 'us-west-2b'
+
+        # for IMDSv2 fail and IMDSv1 success
+        plugin.initialize()
+        assert hasattr(plugin, 'runtime_context')
+        r_c = getattr(plugin, 'runtime_context')
+        assert r_c['instance_id'] == 'i-0a1d026d92d4709ab'
+        assert r_c['availability_zone'] == 'us-west-2a'
+
+        # for both failure
+        plugin.initialize()
+        assert hasattr(plugin, 'runtime_context')
+        r_c = getattr(plugin, 'runtime_context')
+        assert r_c is None
