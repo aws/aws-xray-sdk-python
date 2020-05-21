fe7f8566b2e86be9226c3a40167d84ecc4ccbcb1
srprash <50466688+srprash@users.noreply.github.com>
2020-05-22 01:43:52 +0530

    IMDSv2 support for EC2 plugin (#226)
    * Implemented support for IMDSv2 and fallback to IMDSv1. Added unit tests for the same
    
    * Patching function instead of urlopen. Using mock since its compatible with python27
    
    * Changed the token ttl to 60 seconds to be safe
    
    * Some refactoring
    
    * Removed unnecessary comments from tests

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,4 +1,5 @@
 from aws_xray_sdk.core.plugins.utils import get_plugin_modules
+from mock import patch
 
 supported_plugins = (
     'ec2_plugin',
@@ -13,3 +14,38 @@ def test_runtime_context_available():
     for plugin in plugins:
         plugin.initialize()
         assert hasattr(plugin, 'runtime_context')
+
+
+@patch('aws_xray_sdk.core.plugins.ec2_plugin.do_request')
+def test_ec2_plugin_imdsv2_success(mock_do_request):
+    mock_do_request.side_effect = ['token', 'i-0a1d026d92d4709cd', 'us-west-2b']
+
+    ec2_plugin = get_plugin_modules(('ec2_plugin',))[0]
+    ec2_plugin.initialize()
+    assert hasattr(ec2_plugin, 'runtime_context')
+    r_c = getattr(ec2_plugin, 'runtime_context')
+    assert r_c['instance_id'] == 'i-0a1d026d92d4709cd'
+    assert r_c['availability_zone'] == 'us-west-2b'
+
+
+@patch('aws_xray_sdk.core.plugins.ec2_plugin.do_request')
+def test_ec2_plugin_v2_fail_v1_success(mock_do_request):
+    mock_do_request.side_effect = [Exception("Boom!"), 'i-0a1d026d92d4709ab', 'us-west-2a']
+
+    ec2_plugin = get_plugin_modules(('ec2_plugin',))[0]
+    ec2_plugin.initialize()
+    assert hasattr(ec2_plugin, 'runtime_context')
+    r_c = getattr(ec2_plugin, 'runtime_context')
+    assert r_c['instance_id'] == 'i-0a1d026d92d4709ab'
+    assert r_c['availability_zone'] == 'us-west-2a'
+
+
+@patch('aws_xray_sdk.core.plugins.ec2_plugin.do_request')
+def test_ec2_plugin_v2_fail_v1_fail(mock_do_request):
+    mock_do_request.side_effect = [Exception("Boom v2!"), Exception("Boom v1!")]
+
+    ec2_plugin = get_plugin_modules(('ec2_plugin',))[0]
+    ec2_plugin.initialize()
+    assert hasattr(ec2_plugin, 'runtime_context')
+    r_c = getattr(ec2_plugin, 'runtime_context')
+    assert r_c is None
