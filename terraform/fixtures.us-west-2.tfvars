75678e7633279d7c82889e502019dfcc50d78449
bhautikpip <bhautip@amazon.com>
2020-09-22 10:43:44 -0700

    removed constant vars from tfvars and used env variable to populate values

--- terraform/fixtures.us-west-2.tfvars
+++ terraform/fixtures.us-west-2.tfvars
@@ -1,15 +1,5 @@
 region = "us-west-2"
 
-bucket_name = "${var.resource_prefix}.eb.flask.applicationversion"
-
 bucket_key = "beanstalk/deploy.zip"
 
 source_path = "deploy.zip"
-
-eb_app_name = "${var.resource_prefix}-EB-Flask-App"
-
-eb_app_version_label = "${var.resource_prefix}-EB-Flask-App-1"
-
-eb_env_name = "${var.resource_prefix}-EB-Flask-App-Env"
-
-eb_env_cname_prefix = "${var.resource_prefix}-Eb-flask-app-env"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,11 +3,6 @@ variable "region" {
   description = "AWS region for deployment of resources"
 }
 
-variable "bucket_name" {
-  type        = string
-  description = "AWS s3 bucket name"
-}
-
 variable "bucket_key" {
   type        = string
   description = "AWS s3 object key"
@@ -18,28 +13,5 @@ variable "source_path" {
   description = "local source zip path to upload on AWS s3 bucket"
 }
 
-variable "eb_app_name" {
-  type        = string
-  description = "Elastic Beanstalk application name"
-}
-
-variable "eb_app_version_label" {
-  type        = string
-  description = "Elastic Beanstalk application version label"
-}
-
-variable "eb_env_name" {
-  type        = string
-  description = "Elastic Beanstalk environment name"
-}
-
-variable "eb_env_cname_prefix" {
-  type = string
-  description = "Elastic Beanstalk environment CNAME prefix"
-}
-
-variable "resource_prefix" {
-  type = string
-  description = "provide unique names to terraform resources"
-}
+variable "resource_prefix" {}
 
