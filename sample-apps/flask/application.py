e123319d69450e1c2fd96648879d47c14bfd8e0e
bhautikpip <bhautip@amazon.com>
2020-11-18 10:22:19 -0800

    workflow executes on push

--- sample-apps/flask/application.py
+++ sample-apps/flask/application.py
@@ -1,6 +1,3 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
 import boto3
 from flask import Flask
 from aws_xray_sdk.core import xray_recorder, patch_all
