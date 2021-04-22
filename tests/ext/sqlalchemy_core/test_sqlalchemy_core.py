8d2956a8b24993feb35ef998fd1d69f2537be98f
Prashant Srivastava <srprash@amazon.com>
2021-04-22 13:28:41 -0700

    Splitting out tests

--- tests/ext/sqlalchemy_core/test_sqlalchemy_core.py
+++ tests/ext/sqlalchemy_core/test_sqlalchemy_core.py
@@ -94,16 +94,3 @@ def test_connection_query(connection):
     assert sql_meta['sanitized_query'].startswith('DELETE FROM users')
     assert sql_meta['url'] == 'sqlite:///:memory:'
     assert password not in sql_meta['sanitized_query']
-
-
-# 2.0 style execution test. see https://docs.sqlalchemy.org/en/14/changelog/migration_14.html#orm-query-is-internally
-# -unified-with-select-update-delete-2-0-style-execution-available
-def test_orm_style_select_execution(session):
-    statement = select(User).where(
-        User.name == 'John'
-    )
-    session.execute(statement)
-    assert len(xray_recorder.current_segment().subsegments) == 1
-    sql_meta = xray_recorder.current_segment().subsegments[0].sql
-    assert sql_meta['sanitized_query'].startswith('SELECT')
-    assert 'FROM users' in sql_meta['sanitized_query']
